d8369ba7d659e3aea6aca5e8adf1ac7ab428a1d2
saishankar2748 <saishankar2748@gmail.com>
2022-10-09 02:01:41 +0530

    creating models,connecting to database from contact form

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.CharField(max_length=122)
+    phone = models.CharField(max_length=12)
+    desc = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
--- home/views.py
+++ home/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render,HttpResponse
-
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 # Create your views here.
 def index(request):
     context = {
         'var1':'Sandhya',
         'var2':'Santhosh'
     }
+   # messages.success(request,"this is success mesg")
     return render(request,'index.html',context)
     # return HttpResponse("This is home page")
 
@@ -18,5 +21,14 @@ def service(request):
    # return HttpResponse("Services provided")
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent !!')
+
     return render(request,'contact.html')
     #return HttpResponse("contact us")
